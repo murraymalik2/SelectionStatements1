01ebeba47660d91b0eb4026d941c68acb9e232cf
murra <murraymalik2@gmail.com>
2023-08-08 18:24:25 -0400

    fizzbuzz

--- source/repos/FizzBuzzChallenge/FizzBuzz1.cs
+++ source/repos/FizzBuzzChallenge/FizzBuzz1.cs
@@ -1,21 +1,35 @@
- public string FizzBuzz(int number)
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FizzBuzzChallenge
+//Write a public method that accepts a number 
+//1. access modifier
+//2. return type
+//3. Method Name
+//4. Parameters
+//5. Scope
+
+{
+    public class Program
+    {
+        public string FizzBuzz(int number)
         {
             var answer = "";
 
             //when the number is divisible by 3 return the word fizz
             if (number % 3 == 0) 
             {
-                answer = "fizz";
+                return ($"Fizz");
             }
+            //when the number is divisible by 5 return the word buzz
             if (number % 5 == 0)
             {
-                answer += "buzz"; // answer = answer + "buzz"
-
-                return answer;
-            //when the number is divisible by 5 return the word buzz
-
+                return ($"");
+            }
 
 
             //when the number is divisible by both, return the word fizzbuzz
         }
-        }
+
+    }
+}
